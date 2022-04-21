bc8c34dba522b6e0a1ddfaafa2c7b3a48cead987
Aniket Chaurasia <darkhorse@darkhorse-MacBook-Air.local>
2022-04-21 15:03:28 +0530

    Database URI from env

--- bankapp/models.py
+++ bankapp/models.py
@@ -1,15 +1,11 @@
 import psycopg2
-
+import os
 
 def get_db_connection():
-	# db_con = psycopg2.connect(host="localhost", 
-	#                  port="5432", 
-	#                  user="postgres", 
-	#                  password="asb266", 
-	#                  database="darkhorse", 
-	#                  options="-c search_path=bankdata")
-	# return db_con
-	conn_string = 'postgresql://ufazqe5ru1fnd974uhrv:n1Bq3tI1IUb3nVN7LjTz@bsvjhovusoysfhtsoiec-postgresql.services.clever-cloud.com:5432/bsvjhovusoysfhtsoiec'
+	
+	conn_string = os.environ.get("POSTGRESQL_ADDON_URI")	
+	# conn_string = os.environ.get("DATABASE_URI")
+
 	try:
 		return psycopg2.connect(conn_string)
 	except Exception as e:
@@ -43,9 +39,8 @@ def branch_autocomplete_model(param_search, param_limit, param_offset):
 def branches_model(param_search, param_limit, param_offset):
 	conn = get_db_connection()
 	cur = conn.cursor()
-	# print(param_search.is_numeric())
+
 	if param_search.isdigit() and len(param_search) <= 3:
-		# print(param_search)
 		sql = '''select b1.ifsc, b1.bank_id, b1.branch, b1.address, b1.city, b1.district, b1.state, b2.name 
 		from bankdata.branches b1 left join bankdata.banks b2
 		on b1.bank_id = b2.id 
@@ -65,7 +60,6 @@ def branches_model(param_search, param_limit, param_offset):
 		b1.ifsc like %s order by ifsc limit %s offset %s;'''
 	
 		param_list = [param_search] * 6 + [param_limit, param_offset]
-		# print(param_list)
 	
 	cur.execute(sql ,tuple(param_list))
 	branches = cur.fetchall()
@@ -74,5 +68,4 @@ def branches_model(param_search, param_limit, param_offset):
 
 	if len(branches) == 0:
 		return {'message': 'No data found'}
-	# print(branches, type(branches), len(branches))
 	return branches
